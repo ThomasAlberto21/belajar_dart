c7594bdbf158ff58d591d0c2d78bdde20b0661e8
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-09-21 09:53:57 +0700

    Booleans

---
+++ bin/fundamental_dart/booleans.dart
@@ -0,0 +1,17 @@
+void main() {
+  bool alwaysTrue = true;
+  bool alwaysFalse = false;
+  bool notTrue = !true;
+  bool notFalse = !false;
+  print(alwaysTrue);
+  print(alwaysFalse);
+  print(notTrue);
+  print(notFalse);
+
+  // TODO : Latihan pengkondisian
+  if (true) {
+    print("it's true");
+  } else {
+    print("it's false");
+  }
+}
