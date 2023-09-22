36f06386befdab8530dee87dab930bdfe8f06b48
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-09-22 09:50:05 +0700

    Temperature Conversion

---
+++ bin/latihan/temperature_conversion.dart
@@ -0,0 +1,9 @@
+import 'dart:io';
+
+void main() {
+  stdout.write('Masukkan suhu dalam fahrenheit: ');
+  var fahrenheit = num.parse(stdin.readLineSync()!);
+
+  var celcius = (fahrenheit - 32) * 5 / 9;
+  print('$fahrenheit derajat fahrenheit = $celcius derajat celcius');
+}
