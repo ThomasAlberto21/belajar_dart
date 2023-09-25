ee731133525ac018cf5623535e8ca06a7f03bbe6
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-09-25 08:27:29 +0700

    If Else

--- bin/controlFlow/if_else.dart
+++ bin/controlFlow/if_else.dart
@@ -1,19 +1,33 @@
+// void main() {
+//   var score = 80;
+//
+//   print('Nilai anda ${calculateScore(score)}');
+// }
+//
+// String calculateScore(int score) {
+//   if (score >= 90) {
+//     return 'A';
+//   } else if (score >= 80) {
+//     return 'B';
+//   } else if (score >= 70) {
+//     return 'C';
+//   } else if (score >= 60) {
+//     return 'D';
+//   } else {
+//     return 'E';
+//   }
+// }
+//
+//
+
 void main() {
-  var score = 80;
+  int buyOil;
+  buyOil = 1;
 
-  print('Nilai anda ${calculateScore(score)}');
-}
-
-String calculateScore(int score) {
-  if (score >= 90) {
-    return 'A';
-  } else if (score >= 80) {
-    return 'B';
-  } else if (score >= 70) {
-    return 'C';
-  } else if (score >= 60) {
-    return 'D';
-  } else {
-    return 'E';
+  bool isThereAnyEggs = true;
+  if (isThereAnyEggs) {
+    buyOil = 6;
   }
+
+  print('Buy $buyOil oil');
 }
