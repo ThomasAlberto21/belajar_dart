f486f516329bd2b76c234c886f691ea88b87bd16
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-09-26 21:11:23 +0700

    Cascade Notation

--- bin/oop/04_cascadeNotation/04_cascadeNotation.dart
+++ bin/oop/04_cascadeNotation/04_cascadeNotation.dart
@@ -1,5 +1,5 @@
 // TODO : Dart juga dilengkapi dengan cascade notation atau cascade operator. Operator ini memungkinkan kita untuk melakukan beberapa urutan operasi pada objek yang sama. Kita bisa mengakses property dari object dan menjalankan method di dalamnya bersamaan ketika kita menginstansiasi object. Cascade operator dituliskan dengan dua tanda titik (.. atau ?..).
-import 'person.dart';
+import 'animal.dart';
 
 void main() {
   var personData = Person('', 'Jakarta', 23)
