822e144035ff9a5e83f6918316252c82c408b01b
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-09-26 20:30:46 +0700

    Class

---
+++ bin/oop/01_class.dart
@@ -0,0 +1,31 @@
+class Animal {
+  String name;
+  int age;
+  double weight;
+
+  Animal(this.name, this.age, this.weight);
+
+  void eat() {
+    print("$name is eating");
+    weight += 0.2;
+  }
+
+  void sleep() {
+    print("$name is sleeping");
+  }
+
+  void poop() {
+    print("$name is pooping");
+    weight -= 0.1;
+  }
+}
+
+void main() {
+  var cat = Animal("Caca", 10, 0.3);
+
+  cat.eat();
+  cat.sleep();
+  cat.poop();
+
+  print(cat.weight);
+}
