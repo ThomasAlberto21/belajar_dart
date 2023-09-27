c214ec3ad7ebd58553407b990fe0fcf32cba18ca
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-09-27 08:49:27 +0700

    Abstract Class

---
+++ bin/oop/06_abstractClass/06_abstractClass.dart
@@ -0,0 +1,4 @@
+// TODO : Abstract class adalah kelas yang tidak dapat diinstansiasi, tetapi dapat diturunkan ke kelas lain. Kelas abstrak digunakan untuk mendefinisikan perilaku umum dari kelas-kelas tertentu.
+import 'animal.dart'
+
+var cat = Animal(name, age, weight);
