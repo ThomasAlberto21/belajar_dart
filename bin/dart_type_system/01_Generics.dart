4bf74f48df278e10f2ad0d36d52601a444cd2ac5
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-10-01 12:22:37 +0700

    Generics

---
+++ bin/dart_type_system/01_Generics.dart
@@ -0,0 +1,5 @@
+// TODO :Generics adalah fitur dalam bahasa pemrograman Dart yang memungkinkan kita untuk membuat fungsi, kelas, atau tipe data yang dapat digunakan ulang untuk beberapa tipe data. Dengan menggunakan generics, kita tidak perlu menulis ulang kode yang sama untuk setiap tipe data yang berbeda.
+
+List<int> numberList = [1, 2, 3]; // Hanya untuk tipe data int
+List<String> stringList = ['1', '2', '3']; // Hanya untuk tipe data String
+List<dynamic> dynamicList = [1, '2', 3]; // Untuk semua tipe data
